f0548b3f72ff344630f6ea3b7def9b4ed14dbf63
Pawel Decowski <pawel.decowski@gmail.com>
2011-04-26 13:55:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+jQuery Listify by Pawel Decowski
+================================
+
+About
+-----
+jQuery plugin which transforms an input element into a nice looking list widget.
+
+Licence
+-------
+jQuery Listify is licensed under a [Creative Commons Attribution-ShareAlike 3.0 Unported License](http://creativecommons.org/licenses/by-sa/3.0/).
+
+You are free:
+* to Share — to copy, distribute and transmit the work
+* to Remix — to adapt the work
+
+Under the following conditions:
+* Attribution — You must attribute the work in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work).
+* Share Alike — If you alter, transform, or build upon this work, you may distribute the resulting work only under the same or similar license to this one.
+
---
+++ jquery.listify.js
@@ -0,0 +1,148 @@
+(function($)
+{
+    $.fn.listify = function(suggestions) {
+        var suggestions = suggestions || [];
+
+        var methods = {
+            updateInput: function(listified)
+            {
+                var tagString = "";
+
+                $("li:not(.input)", listified).each(function()
+                {
+                    var tag = $(this).contents().get(0).textContent;
+
+                    tagString += tag + ',';
+                });
+
+                tagString = tagString.substring(0, tagString.length-1);;
+
+                listified.prev("input").val(tagString);
+            },
+
+            updateList: function(input)
+            {
+                if (!input.val().length) {
+                    return;
+                }
+
+                var items = input.val().split(",");
+
+                methods.addItems(input.next(".listify")[0], items)
+            },
+
+            addItems: function(listified, items)
+            {
+                for (var i in items) {
+                    var $item = $('<li>' + items[i] + '<a href="#">&times;</a></li>');
+
+                    $(".input", listified).before($item);
+
+                    $item.click(function(e)
+                    {
+                        e.stopPropagation();
+                    });
+
+                    $("a", $item).click(function(e)
+                    {
+                        e.preventDefault();
+                        e.stopPropagation();
+
+                        $listify = $(this).closest(".listify");
+                        $(this).parent().remove();
+
+                        methods.updateInput($listify);
+                        $listify.find("input").focus();
+                    });
+                }
+            }
+        }
+
+        this.hide();
+        var $list = $('<ul class="listify"><li class="input"><input /></li></ul>');
+        this.after($list);
+
+        methods.updateList(this);
+
+        $list.click(function()
+        {
+            $("input", $list).focus();
+        });
+
+        $("input", this.next(".listify")).keydown(function(e)
+        {
+            $(this).css("width", ($(this).val().length * 0.65 + 1.5)  + "em");
+
+            if ($(this).val() == "" && e.keyCode == "8") {
+                if ($(this).parent().prev("li").hasClass("delete")) {
+                    $(this).parent().prev("li").remove();
+                    methods.updateInput($(this).closest(".listify"));
+                } else {
+                    $(this).parent().prev("li").addClass("delete");
+                }
+            } else {
+                $(this).parent().prev("li").removeClass("delete");
+            }
+
+            if (e.keyCode == "13" || e.keyCode == "9") {
+                if (e.keyCode == "13") {
+                    e.preventDefault();
+                }
+
+                if ($(this).val() == "") {
+                    return;
+                }
+
+                methods.addItems($(this).closest(".listify"), $(this).val().split(","));
+                $(this).val("");
+                methods.updateInput($(this).closest(".listify"));
+            }
+        })
+        .keyup(function(e)
+        {
+            if (e.keyCode == "27") {
+                $(this).val("");
+            }
+
+            $(this).closest('.listify').next(".listify-suggestions").remove();
+
+            $suggestions = $('<ul class="listify-suggestions"></ul>');
+
+            if ($(this).val().length) {
+                for (var i in $(this).data("suggestions")) {
+                    var s = $(this).data("suggestions")[i];
+
+                    var items = $(this).closest(".listify").prev("input").val().split(",");
+
+                    items = items.map(String.toLowerCase);
+
+                    if (items.indexOf(s.toLowerCase()) > -1) {
+                        continue;
+                    }
+
+                    if (s.toLowerCase().indexOf($(this).val().toLowerCase()) > -1) {
+                        re = new RegExp($(this).val(), "gi");
+                        $suggestions.append("<li>" + s.replace(re, "<strong>$&</strong>") + "</li>");
+                    }
+                }
+
+                if ($("li", $suggestions).length) {
+                    $(this).closest('.listify').after($suggestions);
+                    $("li", $suggestions).click(function(e) {
+                        methods.addItems($(this).parent().prev(".listify"), [ $(this).text() ]);
+                        methods.updateInput($(this).parent().prev(".listify"));
+                        $(this).parent().prev(".listify").find("input").val("").focus();
+                        $(this).parent().remove();
+                    });
+                }
+            }
+        })
+        .data('suggestions', suggestions.sort());
+
+        $("body").click(function()
+        {
+            $(".listify-suggestions").remove();
+        });
+    };
+})(jQuery);
+
